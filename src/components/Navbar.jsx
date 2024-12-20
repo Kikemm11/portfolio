0a42ccf7d86b6dd9ae611ece9b54643ff9714295
Kikemm11 <Kikemaldonado11@gmail.com>
2024-12-20 01:10:25 -0400

    Update the routes at Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,16 @@ const Navbar = () => {
       {/* Left: Home Icon */}
       <div className="flex items-center space-x-2">
         <AiFillHome className="text-2xl" />
-        <Link to="/portfolio" className="text-white font-semibold hover:text-gray-300">
+        <Link to="/" className="text-white font-semibold hover:text-gray-300">
           Home
         </Link>
       </div>
 
       {/* Right: Buttons */}
       <div className="flex space-x-4">
-        <NavbarButton content={"Skills"} link={"/portfolio/skills"} />
-        <NavbarButton content={"Projects"} link={"/portfolio/projects"} />
-        <NavbarButton content={"Contact"} link={"/portfolio/contact"} />
+        <NavbarButton content={"Skills"} link={"/skills"} />
+        <NavbarButton content={"Projects"} link={"/projects"} />
+        <NavbarButton content={"Contact"} link={"/contact"} />
       </div>
     </nav>
   );
