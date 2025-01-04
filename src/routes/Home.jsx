d8ba834253b781232c196ecf9d3601edc48b2a36
Kikemm11 <Kikemaldonado11@gmail.com>
2025-01-04 17:53:53 -0400

    Add responsive details to the Home route

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -13,9 +13,9 @@ function Home() {
         <p className="text-xl font-italic font-arial mt-2 text-white">Iván Maldonado | Software Developer</p>
       </div>
 
-      <div className="flex flex-col lg:flex-row justify-center items-center mt-40 mr-20">
+      <div className="flex flex-col lg:flex-row justify-center items-center mt-40 lg:mr-20">
         {/* Left Section: Image */}
-        <div className="lg:w-1/3 flex justify-center">
+        <div className="lg:w-1/3 flex justify-center mb-8 lg:mb-0">
           <img
             src={meImage}
             alt="Me"
@@ -24,10 +24,10 @@ function Home() {
         </div>
 
         {/* Right Section: About Me */}
-        <div className="lg:w-2/3 flex flex-col justify-center">
+        <div className="lg:w-2/3 flex flex-col justify-center items-center text-center lg:text-left">
           <h2 className="text-5xl font-poppins mb-4 text-white">About Me</h2>
-          <div className="bg-white/80 hover:bg-white/90 p-4 rounded-xl shadow-md w-full max-w-6xl mx-auto flex flex-col justify-center items-center transition-opacity duration-300">
-            <p className="text-lg text-gray-700 text-left">
+          <div className="bg-white/80 hover:bg-white/90 p-4 rounded-xl shadow-md w-full max-w-6xl flex flex-col transition-opacity duration-300">
+            <p className="text-lg text-gray-700">
               I'm a Backend-Oriented Software Developer with over 4 years of experience, currently finishing a Systems Engineering degree in the University of Los Andes, Mérida-Venezuela. 
               I have worked on multiple projects, taking on different roles that have allowed me to apply and refine my technical skills and process modeling 
               expertise in a comprehensive manner. My academic background and professional experience have equipped me to take complex challenges and deliver 
@@ -39,7 +39,7 @@ function Home() {
             </p>
           </div>
         </div>
-        </div>
+      </div>
 
       {/* Footer Quote */}
       <footer className="mt-40 mr-10 text-right text-white text-sm font-poppins italic self-end">
